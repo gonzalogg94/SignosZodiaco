2aa0e67fd77785cee2423fd978076e011179e8a8
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-08-30 20:56:44 -0300

    realize modificaciones en documentos html para poder trabajar la logica

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
-
-
+// declaracion de variables 
 let formulario = document.querySelector('#formZodiaco');
+let fechaNacimiento = document.querySelector('#fechaNacimiento');
+let elemento=document.querySelector("#selectorElemento")
+
+// agrego los eventos 
+formulario.addEventListener('submit', mostrarFecha)
+
+// funciones 
+function mostrarFecha(e){
+   e.preventDefault();
+   console.log(fechaNacimiento.value)
+   console.log(elemento.value)
+}
