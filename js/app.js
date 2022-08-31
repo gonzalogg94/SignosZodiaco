9ff16f29dc64b65f3865ddabf347933758f707c9
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-08-30 21:50:27 -0300

    agregue funcionalidad al selector de elementos del zodiaco

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 // declaracion de variables 
 let formulario = document.querySelector('#formZodiaco');
 let fechaNacimiento = document.querySelector('#fechaNacimiento');
-let elemento=document.querySelector("#selectorElemento")
+let elemento=document.querySelector("#elemento")
+let btnmostrarDatos = document.querySelector("#elegirElemento");
 
 // agrego los eventos 
 formulario.addEventListener('submit', mostrarFecha)
@@ -10,5 +11,52 @@ formulario.addEventListener('submit', mostrarFecha)
 function mostrarFecha(e){
    e.preventDefault();
    console.log(fechaNacimiento.value)
-   console.log(elemento.value)
 }
+
+function elegirElemento(elemento){
+
+}
+
+btnmostrarDatos.addEventListener("click", () =>
+      mostrarElemento()
+    );
+
+    function mostrarElemento(){
+      if(elemento.value==1){
+         let elementoAgua = document.querySelector("#elementoAgua");
+         elementoAgua.className = "row mt-5 colorAgua my-3";
+         let elementoFuego = document.querySelector("#elementoFuego");
+      elementoFuego.className = "row my-5 colorFuego d-none";
+      let elementoTierra = document.querySelector("#elementoTierra");
+      elementoTierra.className = "row mt-5 colorTierra d-none";
+      let elementoAire = document.querySelector("#elementoAire");
+      elementoAire.className = "row mt-5 colorAire d-none";
+      }else if(elemento.value==2){
+         let elementoAgua = document.querySelector("#elementoAgua");
+         elementoAgua.className = "row mt-5 colorAgua my-3 d-none";
+         let elementoFuego = document.querySelector("#elementoFuego");
+      elementoFuego.className = "row my-5 colorFuego";
+      let elementoTierra = document.querySelector("#elementoTierra");
+      elementoTierra.className = "row mt-5 colorTierra d-none";
+      let elementoAire = document.querySelector("#elementoAire");
+      elementoAire.className = "row mt-5 colorAire d-none";
+      }else if(elemento.value==3){
+         let elementoAgua = document.querySelector("#elementoAgua");
+         elementoAgua.className = "row mt-5 colorAgua my-3 d-none";
+         let elementoFuego = document.querySelector("#elementoFuego");
+      elementoFuego.className = "row my-5 colorFuego d-none";
+      let elementoTierra = document.querySelector("#elementoTierra");
+      elementoTierra.className = "row mt-5 colorTierra";
+      let elementoAire = document.querySelector("#elementoAire");
+      elementoAire.className = "row mt-5 colorAire d-none";
+      }else if(elemento.value==4){
+         let elementoAgua = document.querySelector("#elementoAgua");
+         elementoAgua.className = "row mt-5 colorAgua my-3 d-none";
+         let elementoFuego = document.querySelector("#elementoFuego");
+      elementoFuego.className = "row my-5 colorFuego d-none";
+      let elementoTierra = document.querySelector("#elementoTierra");
+      elementoTierra.className = "row mt-5 colorTierra d-none";
+      let elementoAire = document.querySelector("#elementoAire");
+      elementoAire.className = "row mt-5 colorAire";
+    }
+   }
