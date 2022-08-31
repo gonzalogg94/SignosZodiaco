e2f643e8261da79595b7ebe89bf74862ecf6061c
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-08-31 20:10:15 -0300

    manipule el input date para trabajarlo como number

--- js/app.js
+++ js/app.js
@@ -1,75 +1,74 @@
-// declaracion de variables 
-let formulario = document.querySelector('#formZodiaco');
-let fechaNacimiento = document.querySelector('#fechaNacimiento');
-let elemento=document.querySelector("#elemento")
+// declaracion de variables
+let formulario = document.querySelector("#formZodiaco");
+let fechaNacimiento = document.querySelector("#fechaNacimiento");
+let elemento = document.querySelector("#elemento");
 let btnmostrarDatos = document.querySelector("#elegirElemento");
-// agrego los eventos 
-formulario.addEventListener('submit', mostrarFecha)
 
-// funciones 
-function mostrarFecha(e){
-   e.preventDefault();
+  
+
+// Eventos
+formulario.addEventListener("submit", mostrarFecha);
+btnmostrarDatos.addEventListener("click", () => mostrarElemento());
+
+// Funciones
+function mostrarFecha(e) {
+  e.preventDefault();
+let nacimiento=fechaNacimiento.value
+let diaNacimiento=parseInt(nacimiento.substring(8,10));
+let mesNacimiento=parseInt(nacimiento.substring(5,7));
+  if()
 }
 
-
-function elegirElemento(elemento){
-
+function mostrarElemento() {
+  if (elemento.value == 1) {
+    let elementoAgua = document.querySelector("#elementoAgua");
+    elementoAgua.className = "row mt-5 colorAgua my-3";
+    let elementoFuego = document.querySelector("#elementoFuego");
+    elementoFuego.className = "row my-5 colorFuego d-none";
+    let elementoTierra = document.querySelector("#elementoTierra");
+    elementoTierra.className = "row mt-5 colorTierra d-none";
+    let elementoAire = document.querySelector("#elementoAire");
+    elementoAire.className = "row mt-5 colorAire d-none";
+  } else if (elemento.value == 2) {
+    let elementoAgua = document.querySelector("#elementoAgua");
+    elementoAgua.className = "row mt-5 colorAgua my-3 d-none";
+    let elementoFuego = document.querySelector("#elementoFuego");
+    elementoFuego.className = "row my-5 colorFuego";
+    let elementoTierra = document.querySelector("#elementoTierra");
+    elementoTierra.className = "row mt-5 colorTierra d-none";
+    let elementoAire = document.querySelector("#elementoAire");
+    elementoAire.className = "row mt-5 colorAire d-none";
+  } else if (elemento.value == 3) {
+    let elementoAgua = document.querySelector("#elementoAgua");
+    elementoAgua.className = "row mt-5 colorAgua my-3 d-none";
+    let elementoFuego = document.querySelector("#elementoFuego");
+    elementoFuego.className = "row my-5 colorFuego d-none";
+    let elementoTierra = document.querySelector("#elementoTierra");
+    elementoTierra.className = "row mt-5 colorTierra";
+    let elementoAire = document.querySelector("#elementoAire");
+    elementoAire.className = "row mt-5 colorAire d-none";
+  } else if (elemento.value == 4) {
+    let elementoAgua = document.querySelector("#elementoAgua");
+    elementoAgua.className = "row mt-5 colorAgua my-3 d-none";
+    let elementoFuego = document.querySelector("#elementoFuego");
+    elementoFuego.className = "row my-5 colorFuego d-none";
+    let elementoTierra = document.querySelector("#elementoTierra");
+    elementoTierra.className = "row mt-5 colorTierra d-none";
+    let elementoAire = document.querySelector("#elementoAire");
+    elementoAire.className = "row mt-5 colorAire";
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Debe seleccionar un elemento",
+    });
+    let elementoAgua = document.querySelector("#elementoAgua");
+    elementoAgua.className = "row mt-5 colorAgua my-3 d-none";
+    let elementoFuego = document.querySelector("#elementoFuego");
+    elementoFuego.className = "row my-5 colorFuego d-none";
+    let elementoTierra = document.querySelector("#elementoTierra");
+    elementoTierra.className = "row mt-5 colorTierra d-none";
+    let elementoAire = document.querySelector("#elementoAire");
+    elementoAire.className = "row mt-5 colorAire d-none";
+  }
 }
-
-btnmostrarDatos.addEventListener("click", () =>
-      mostrarElemento()
-    );
-
-    function mostrarElemento(){
-      if(elemento.value==1){
-         let elementoAgua = document.querySelector("#elementoAgua");
-         elementoAgua.className = "row mt-5 colorAgua my-3";
-         let elementoFuego = document.querySelector("#elementoFuego");
-      elementoFuego.className = "row my-5 colorFuego d-none";
-      let elementoTierra = document.querySelector("#elementoTierra");
-      elementoTierra.className = "row mt-5 colorTierra d-none";
-      let elementoAire = document.querySelector("#elementoAire");
-      elementoAire.className = "row mt-5 colorAire d-none";
-      }else if(elemento.value==2){
-         let elementoAgua = document.querySelector("#elementoAgua");
-         elementoAgua.className = "row mt-5 colorAgua my-3 d-none";
-         let elementoFuego = document.querySelector("#elementoFuego");
-      elementoFuego.className = "row my-5 colorFuego";
-      let elementoTierra = document.querySelector("#elementoTierra");
-      elementoTierra.className = "row mt-5 colorTierra d-none";
-      let elementoAire = document.querySelector("#elementoAire"); 
-      elementoAire.className = "row mt-5 colorAire d-none";
-      }else if(elemento.value==3){
-         let elementoAgua = document.querySelector("#elementoAgua");
-         elementoAgua.className = "row mt-5 colorAgua my-3 d-none";
-         let elementoFuego = document.querySelector("#elementoFuego");
-      elementoFuego.className = "row my-5 colorFuego d-none";
-      let elementoTierra = document.querySelector("#elementoTierra");
-      elementoTierra.className = "row mt-5 colorTierra";
-      let elementoAire = document.querySelector("#elementoAire");
-      elementoAire.className = "row mt-5 colorAire d-none";
-      }else if(elemento.value==4){
-         let elementoAgua = document.querySelector("#elementoAgua");
-         elementoAgua.className = "row mt-5 colorAgua my-3 d-none";
-         let elementoFuego = document.querySelector("#elementoFuego");
-      elementoFuego.className = "row my-5 colorFuego d-none";
-      let elementoTierra = document.querySelector("#elementoTierra");
-      elementoTierra.className = "row mt-5 colorTierra d-none";
-      let elementoAire = document.querySelector("#elementoAire");
-      elementoAire.className = "row mt-5 colorAire";
-    }else{
-      Swal.fire({
-         icon: 'error',
-         title: 'Oops...',
-         text: 'Debe seleccionar un elemento',
-       })
-       let elementoAgua = document.querySelector("#elementoAgua");
-         elementoAgua.className = "row mt-5 colorAgua my-3 d-none";
-         let elementoFuego = document.querySelector("#elementoFuego");
-      elementoFuego.className = "row my-5 colorFuego d-none";
-      let elementoTierra = document.querySelector("#elementoTierra");
-      elementoTierra.className = "row mt-5 colorTierra d-none";
-      let elementoAire = document.querySelector("#elementoAire");
-      elementoAire.className = "row mt-5 colorAire d-none";       
-    }
-   }
