d3f7db574711be390d817f63750cd34a5712c20b
Alvaro Ruiz Mejia <zhuone@hotmail.com>
2022-09-01 20:46:18 -0300

    agregada la logica de seleccion de informacion de signos

---
+++ js/app.js
@@ -0,0 +1,212 @@
+//Signos
+let signoAries = document.querySelector("#signoAries");
+let signoTauro = document.querySelector("#signoTauro");
+let signoGeminis = document.querySelector("#signoGeminis");
+let signoCancer = document.querySelector("#signoCancer");
+let signoLeo = document.querySelector("#signoLeo");
+let signoVirgo = document.querySelector("#signoVirgo");
+let signoLibra = document.querySelector("#signoLibra");
+let signoEscorpio = document.querySelector("#signoEscorpio");
+let signoSagitario = document.querySelector("#signoSagitario");
+let signoCapricornio = document.querySelector("#signoCapricornio");
+let signoAcuario = document.querySelector("#signoAcuario");
+let signoPiscis = document.querySelector("#signoPiscis");
+
+//Signos card
+
+let aries = document.querySelector("#infoAries");
+let tauro = document.querySelector("#infoTauro");
+let geminis = document.querySelector("#infoGeminis");
+let cancer = document.querySelector("#infoCancer");
+let leo = document.querySelector("#infoLeo");
+let virgo = document.querySelector("#infoVirgo");
+let libra = document.querySelector("#infoLibra");
+let escorpio = document.querySelector("#infoEscorpio");
+let sagitario = document.querySelector("#infoSagitario");
+let capricornio = document.querySelector("#infoCapricornio");
+let acuario = document.querySelector("#infoAcuario");
+let piscis = document.querySelector("#infoPiscis");
+
+signoAries.addEventListener("click", ariesInfo);
+signoTauro.addEventListener("click", tauroInfo);
+signoGeminis.addEventListener("click", geminisInfo);
+signoCancer.addEventListener("click", cancerInfo);
+signoLeo.addEventListener("click", leoInfo);
+signoVirgo.addEventListener("click", virgoInfo);
+signoLibra.addEventListener("click", libraInfo);
+signoEscorpio.addEventListener("click", escorpioInfo);
+signoSagitario.addEventListener("click", sagitarioInfo);
+signoCapricornio.addEventListener("click", capricornioInfo);
+signoAcuario.addEventListener("click", acuarioInfo);
+signoPiscis.addEventListener("click", piscisInfo);
+
+function ariesInfo() {
+  aries.className = "my-5 card rounded-4 border-dark"
+   tauro.className = "my-5 card rounded-4 border-dark d-none"
+   geminis.className = "my-5 card rounded-4 border-dark d-none"
+   cancer.className = "my-5 card rounded-4 border-dark d-none"
+   leo.className = "my-5 card rounded-4 border-dark d-none"
+   virgo.className = "my-5 card rounded-4 border-dark d-none"
+   libra.className = "my-5 card rounded-4 border-dark d-none"
+   escorpio.className = "my-5 card rounded-4 border-dark d-none"
+   sagitario.className = "my-5 card rounded-4 border-dark d-none"
+   capricornio.className = "my-5 card rounded-4 border-dark d-none"
+   acuario.className = "my-5 card rounded-4 border-dark d-none"
+   piscis.className = "my-5 card rounded-4 border-dark d-none"
+}
+
+function tauroInfo() {
+  aries.className = "my-5 card rounded-4 border-dark d-none"
+   tauro.className = "my-5 card rounded-4 border-dark"
+   geminis.className = "my-5 card rounded-4 border-dark d-none"
+   cancer.className = "my-5 card rounded-4 border-dark d-none"
+   leo.className = "my-5 card rounded-4 border-dark d-none"
+   virgo.className = "my-5 card rounded-4 border-dark d-none"
+   libra.className = "my-5 card rounded-4 border-dark d-none"
+   escorpio.className = "my-5 card rounded-4 border-dark d-none"
+   sagitario.className = "my-5 card rounded-4 border-dark d-none"
+   capricornio.className = "my-5 card rounded-4 border-dark d-none"
+   acuario.className = "my-5 card rounded-4 border-dark d-none"
+   piscis.className = "my-5 card rounded-4 border-dark d-none"
+}
+function geminisInfo() {
+  aries.className = "my-5 card rounded-4 border-dark d-none"
+   tauro.className = "my-5 card rounded-4 border-dark d-none"
+   geminis.className = "my-5 card rounded-4 border-dark"
+   cancer.className = "my-5 card rounded-4 border-dark d-none"
+   leo.className = "my-5 card rounded-4 border-dark d-none"
+   virgo.className = "my-5 card rounded-4 border-dark d-none"
+   libra.className = "my-5 card rounded-4 border-dark d-none"
+   escorpio.className = "my-5 card rounded-4 border-dark d-none"
+   sagitario.className = "my-5 card rounded-4 border-dark d-none"
+   capricornio.className = "my-5 card rounded-4 border-dark d-none"
+   acuario.className = "my-5 card rounded-4 border-dark d-none"
+   piscis.className = "my-5 card rounded-4 border-dark d-none"
+}
+function cancerInfo() {
+  aries.className = "my-5 card rounded-4 border-dark d-none"
+   tauro.className = "my-5 card rounded-4 border-dark d-none"
+   geminis.className = "my-5 card rounded-4 border-dark d-none"
+   cancer.className = "my-5 card rounded-4 border-dark"
+   leo.className = "my-5 card rounded-4 border-dark d-none"
+   virgo.className = "my-5 card rounded-4 border-dark d-none"
+   libra.className = "my-5 card rounded-4 border-dark d-none"
+   escorpio.className = "my-5 card rounded-4 border-dark d-none"
+   sagitario.className = "my-5 card rounded-4 border-dark d-none"
+   capricornio.className = "my-5 card rounded-4 border-dark d-none"
+   acuario.className = "my-5 card rounded-4 border-dark d-none"
+   piscis.className = "my-5 card rounded-4 border-dark d-none"
+}
+function leoInfo() {
+  aries.className = "my-5 card rounded-4 border-dark d-none"
+   tauro.className = "my-5 card rounded-4 border-dark d-none"
+   geminis.className = "my-5 card rounded-4 border-dark d-none"
+   cancer.className = "my-5 card rounded-4 border-dark d-none"
+   leo.className = "my-5 card rounded-4 border-dark"
+   virgo.className = "my-5 card rounded-4 border-dark d-none"
+   libra.className = "my-5 card rounded-4 border-dark d-none"
+   escorpio.className = "my-5 card rounded-4 border-dark d-none"
+   sagitario.className = "my-5 card rounded-4 border-dark d-none"
+   capricornio.className = "my-5 card rounded-4 border-dark d-none"
+   acuario.className = "my-5 card rounded-4 border-dark d-none"
+   piscis.className = "my-5 card rounded-4 border-dark d-none"
+}
+function virgoInfo() {
+  aries.className = "my-5 card rounded-4 border-dark d-none"
+   tauro.className = "my-5 card rounded-4 border-dark d-none"
+   geminis.className = "my-5 card rounded-4 border-dark d-none"
+   cancer.className = "my-5 card rounded-4 border-dark d-none"
+   leo.className = "my-5 card rounded-4 border-dark d-none"
+   virgo.className = "my-5 card rounded-4 border-dark"
+   libra.className = "my-5 card rounded-4 border-dark d-none"
+   escorpio.className = "my-5 card rounded-4 border-dark d-none"
+   sagitario.className = "my-5 card rounded-4 border-dark d-none"
+   capricornio.className = "my-5 card rounded-4 border-dark d-none"
+   acuario.className = "my-5 card rounded-4 border-dark d-none"
+   piscis.className = "my-5 card rounded-4 border-dark d-none"
+}
+function libraInfo() {
+  aries.className = "my-5 card rounded-4 border-dark d-none"
+   tauro.className = "my-5 card rounded-4 border-dark d-none"
+   geminis.className = "my-5 card rounded-4 border-dark d-none"
+   cancer.className = "my-5 card rounded-4 border-dark d-none"
+   leo.className = "my-5 card rounded-4 border-dark d-none"
+   virgo.className = "my-5 card rounded-4 border-dark d-none"
+   libra.className = "my-5 card rounded-4 border-dark"
+   escorpio.className = "my-5 card rounded-4 border-dark d-none"
+   sagitario.className = "my-5 card rounded-4 border-dark d-none"
+   capricornio.className = "my-5 card rounded-4 border-dark d-none"
+   acuario.className = "my-5 card rounded-4 border-dark d-none"
+   piscis.className = "my-5 card rounded-4 border-dark d-none"
+}
+function escorpioInfo() {
+  aries.className = "my-5 card rounded-4 border-dark d-none"
+   tauro.className = "my-5 card rounded-4 border-dark d-none"
+   geminis.className = "my-5 card rounded-4 border-dark d-none"
+   cancer.className = "my-5 card rounded-4 border-dark d-none"
+   leo.className = "my-5 card rounded-4 border-dark d-none"
+   virgo.className = "my-5 card rounded-4 border-dark d-none"
+   libra.className = "my-5 card rounded-4 border-dark d-none"
+   escorpio.className = "my-5 card rounded-4 border-dark"
+   sagitario.className = "my-5 card rounded-4 border-dark d-none"
+   capricornio.className = "my-5 card rounded-4 border-dark d-none"
+   acuario.className = "my-5 card rounded-4 border-dark d-none"
+   piscis.className = "my-5 card rounded-4 border-dark d-none"
+}
+function sagitarioInfo() {
+  aries.className = "my-5 card rounded-4 border-dark d-none"
+   tauro.className = "my-5 card rounded-4 border-dark  d-none"
+   geminis.className = "my-5 card rounded-4 border-dark d-none"
+   cancer.className = "my-5 card rounded-4 border-dark d-none"
+   leo.className = "my-5 card rounded-4 border-dark d-none"
+   virgo.className = "my-5 card rounded-4 border-dark d-none"
+   libra.className = "my-5 card rounded-4 border-dark d-none"
+   escorpio.className = "my-5 card rounded-4 border-dark d-none"
+   sagitario.className = "my-5 card rounded-4 border-dark"
+   capricornio.className = "my-5 card rounded-4 border-dark d-none"
+   acuario.className = "my-5 card rounded-4 border-dark d-none"
+   piscis.className = "my-5 card rounded-4 border-dark d-none"
+}
+function capricornioInfo() {
+  aries.className = "my-5 card rounded-4 border-dark d-none"
+   tauro.className = "my-5 card rounded-4 border-dark  d-none"
+   geminis.className = "my-5 card rounded-4 border-dark d-none"
+   cancer.className = "my-5 card rounded-4 border-dark d-none"
+   leo.className = "my-5 card rounded-4 border-dark d-none"
+   virgo.className = "my-5 card rounded-4 border-dark d-none"
+   libra.className = "my-5 card rounded-4 border-dark d-none"
+   escorpio.className = "my-5 card rounded-4 border-dark d-none"
+   sagitario.className = "my-5 card rounded-4 border-dark d-none"
+   capricornio.className = "my-5 card rounded-4 border-dark"
+   acuario.className = "my-5 card rounded-4 border-dark d-none"
+   piscis.className = "my-5 card rounded-4 border-dark d-none"
+}
+function acuarioInfo() {
+  aries.className = "my-5 card rounded-4 border-dark d-none"
+   tauro.className = "my-5 card rounded-4 border-dark d-none"
+   geminis.className = "my-5 card rounded-4 border-dark d-none"
+   cancer.className = "my-5 card rounded-4 border-dark d-none"
+   leo.className = "my-5 card rounded-4 border-dark d-none"
+   virgo.className = "my-5 card rounded-4 border-dark d-none"
+   libra.className = "my-5 card rounded-4 border-dark d-none"
+   escorpio.className = "my-5 card rounded-4 border-dark d-none"
+   sagitario.className = "my-5 card rounded-4 border-dark d-none"
+   capricornio.className = "my-5 card rounded-4 border-dark d-none"
+   acuario.className = "my-5 card rounded-4 border-dark"
+   piscis.className = "my-5 card rounded-4 border-dark d-none"
+}
+function piscisInfo() {
+  aries.className = "my-5 card rounded-4 border-dark d-none"
+   tauro.className = "my-5 card rounded-4 border-dark d-none"
+   geminis.className = "my-5 card rounded-4 border-dark d-none"
+   cancer.className = "my-5 card rounded-4 border-dark d-none"
+   leo.className = "my-5 card rounded-4 border-dark d-none"
+   virgo.className = "my-5 card rounded-4 border-dark d-none"
+   libra.className = "my-5 card rounded-4 border-dark d-none"
+   escorpio.className = "my-5 card rounded-4 border-dark d-none"
+   sagitario.className = "my-5 card rounded-4 border-dark d-none"
+   capricornio.className = "my-5 card rounded-4 border-dark d-none"
+   acuario.className = "my-5 card rounded-4 border-dark d-none"
+   piscis.className = "my-5 card rounded-4 border-dark"
+}
+
